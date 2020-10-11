a69e5fa7fda88a1b3c2e2aa66a5fefa01e65b69e
Mamathakamath <mamathakamath94@gmail.com>
2020-10-11 19:46:44 +0530

    Included Basic DI Implementation

---
+++ DependencyInjection/DI.BAL/UnitOfWork/GetOutputFromFileSystem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DI.BAL.UnitOfWork
+{
+    public class GetOutputFromFileSystem
+    {
+        public string FileSystemCall()
+        {
+            return "From File System";
+        }
+    }
+}
---
+++ WebAPI/Controllers/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using DI.BAL.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class DependencyInjectionController : ControllerBase
+    {
+        private readonly IGetOutput _iGetOutput;
+
+        public DependencyInjectionController(IGetOutput iGetOutput)
+        {
+            _iGetOutput = iGetOutput;
+        }
+
+        [HttpPost]
+        public IActionResult GetOutputSource(string InputType)
+        {
+            return Ok(_iGetOutput.GetOutput(InputType));
+        }
+    }
+}
